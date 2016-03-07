7682cd79ef89de834c4983e11385614f708e2836
alemc2 <alemc2@gmail.com>
2016-03-07 00:36:36 -0500

    Adding Relu layer at end and pooling at beginning

--- assign2/models/paper.lua
+++ assign2/models/paper.lua
@@ -1,27 +1,29 @@
 require 'nn'
 
 local model = nn.Sequential()
-local poolsize = 4
+local poolsize = 2
 local nstates = {96,64}
 local filtsize = {13,5}
 local group_size = 4
 
 model:add(nn.SpatialBatchNormalization(nstates[1]))
 model:add(nn.ReLU())
---model:add(nn.SpatialMaxPooling(poolsize, poolsize, poolsize, poolsize))
+model:add(nn.SpatialMaxPooling(poolsize, poolsize, poolsize, poolsize))
 
-local mapping = nn.tables.random(nstates[1],nstates[1],24)
 model:add(nn.SpatialConvolution(nstates[1],nstates[1],1,1))
-model:add(nn.Reshape(nstates[1]/group_size,group_size,21,21))
+model:add(nn.Reshape(nstates[1]/group_size,group_size,10,10))
 
 multiconv = nn.Parallel(2,2)
 for i = 1,(nstates[1]/group_size) do
-    multiconv:add(nn.SpatialConvolution(group_size,nstates[2],filtsize[2],filtsize[2]))
+    local seq_container = nn.Sequential()
+    seq_container:add(nn.SpatialConvolution(group_size,nstates[2],filtsize[2],filtsize[2]))
+    seq_container:add(nn.ReLU())
+    multiconv:add(seq_container)
 end
 
 model:add(multiconv)
 
-model:add(nn.View(nstates[2]*(nstates[1]/group_size)*17*17))
+model:add(nn.View(nstates[2]*(nstates[1]/group_size)*6*6))
 --model:add(nn.Dropout(0.5))
-model:add(nn.Linear(nstates[2]*(nstates[1]/group_size)*17*17, 10))
+model:add(nn.Linear(nstates[2]*(nstates[1]/group_size)*6*6, 10))
 return model
