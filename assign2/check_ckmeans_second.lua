4bf17f94726dfd8a680fa6586228e18d9be32c6b
Ankit Vani <a@nevitus.org>
2016-03-07 03:03:33 -0500

    save centroids to disk

--- assign2/check_ckmeans_second.lua
+++ assign2/check_ckmeans_second.lua
@@ -5,16 +5,29 @@ require 'ckmeans_enh'
 dofile './provider_second.lua'
 c = require 'trepl.colorize'
 
+cmd = torch.CmdLine()
+cmd:text()
+cmd:text('Options:')
+cmd:option('-group', 999, 'Group number')
+cmd:option('-numWindows', 100000, 'Number of windows')
+cmd:option('-batchSize', 2000, 'Batch size')
+cmd:option('-saveFilePrefix', 'models/second', 'Save filename prefix')
+cmd:text()
+opt = cmd:parse(arg or {})
+
+saveFile = opt.saveFilePrefix .. '_' .. opt.group .. '.t7'
+print('will save kernels to '..saveFile)
+
 torch.setnumthreads(8)
 
-print(c.blue '==>' ..' loading data')
-provider_second = Provider_Second()
+print ('==> Group '.. opt.group)
+provider_second = Provider_Second(opt.group)
 collectgarbage()
 
 data_dim = {4,10,10}
 pSize = 5
 trsize = provider_second.secondData:size()
-numWindows = 50000
+numWindows = opt.numWindows
 ngroups = 24
 
 print(c.blue "==> find clusters")
@@ -45,10 +58,13 @@ function dispfilters (step,c_kernels,c_counts)
    resized_kernels = c_kernels
 end
 
-for i=1,ngroups do
-   print ('==> Group '.. i)
-   provider_second.group = i
-   kernels, counts = unsup.ckmeans(provider_second, ncentroids, data_dim[1], pSize, pSize , numWindows, 15, 10000, dispfilters, true)
-   resized_kernels = dispfilters(10,kernels,counts)
-end
+kernels, counts = unsup.ckmeans(provider_second, ncentroids, data_dim[1], pSize, pSize , numWindows, 15, opt.batchSize, dispfilters, true)
+resized_kernels = dispfilters(10,kernels,counts)
 
+print('==> saving centroids to disk: ' .. saveFile)
+obj = {
+    resized_kernels = resized_kernels,
+    kernels = kernels,
+    counts = counts
+}
+torch.save(saveFile, obj)
