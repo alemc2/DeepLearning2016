64384b7b3464369e9cc2fe5cf73a74532bc9bea9
alemc2 <alemc2@gmail.com>
2016-03-07 00:03:22 -0500

    Changing to spatial convolution instead of SpatialConvolutionMap and adjusting Parallel for batch dimension

--- assign2/models/paper.lua
+++ assign2/models/paper.lua
@@ -11,10 +11,10 @@ model:add(nn.ReLU())
 --model:add(nn.SpatialMaxPooling(poolsize, poolsize, poolsize, poolsize))
 
 local mapping = nn.tables.random(nstates[1],nstates[1],24)
-model:add(nn.SpatialConvolutionMap(mapping,1,1))
+model:add(nn.SpatialConvolution(nstates[1],nstates[1],1,1))
 model:add(nn.Reshape(nstates[1]/group_size,group_size,21,21))
 
-multiconv = nn.Parallel(1,1)
+multiconv = nn.Parallel(2,2)
 for i = 1,(nstates[1]/group_size) do
     multiconv:add(nn.SpatialConvolution(group_size,nstates[2],filtsize[2],filtsize[2]))
 end
