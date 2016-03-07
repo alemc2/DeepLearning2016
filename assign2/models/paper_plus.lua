7a71ca7d22e655e775f90821f9d81022b98bb360
Ankit Vani <a@nevitus.org>
2016-03-07 16:44:11 -0500

    Added a new model based on paper

--- assign2/models/paper_plus.lua
+++ assign2/models/paper_plus.lua
@@ -2,8 +2,8 @@ require 'nn'
 
 local model = nn.Sequential()
 local poolsize = 2
-local nstates = {96,64}
-local filtsize = {13,5}
+local nstates = {96,64,512,2048,256,10}
+local filtsize = {13,5,3}
 local group_size = 4
 
 -- usually, we will get secondLayer from classifier.lua
@@ -26,7 +26,7 @@ model:add(nn.SpatialMaxPooling(poolsize, poolsize, poolsize, poolsize))
 model:add(connLayer)
 model:add(nn.Reshape(nstates[1]/group_size,group_size,10,10))
 
-model:add(nn.Dropout(0.5))
+model:add(nn.Dropout(0.4))
 
 multiconv = nn.Parallel(2,2)
 for i = 1,(nstates[1]/group_size) do
@@ -38,9 +38,24 @@ for i = 1,(nstates[1]/group_size) do
     seq_container:add(nn.ReLU())
     multiconv:add(seq_container)
 end
-
 model:add(multiconv)
 
-model:add(nn.View(nstates[2]*(nstates[1]/group_size)*6*6))
-model:add(nn.Linear(nstates[2]*(nstates[1]/group_size)*6*6, 10))
+model:add(nn.Dropout(0.5))
+
+model:add(nn.Reshape((nstates[1]/group_size)*nstates[2],6,6))
+model:add(nn.SpatialConvolution((nstates[1]/group_size)*nstates[2],nstates[3],filtsize[3],filtsize[3]))
+model:add(nn.SpatialBatchNormalization(nstates[3]))
+model:add(nn.ReLU())
+
+model:add(nn.Reshape(nstates[3]*(6-filtsize[3]+1)*(6-filtsize[3]+1)))
+model:add(nn.Linear(nstates[3]*(6-filtsize[3]+1)*(6-filtsize[3]+1), nstates[4]))
+model:add(nn.ReLU())
+
+model:add(nn.Dropout(0.5))
+
+model:add(nn.Linear(nstates[4], nstates[5]))
+model:add(nn.ReLU())
+
+model:add(nn.Linear(nstates[5], nstates[6]))
+
 return model
